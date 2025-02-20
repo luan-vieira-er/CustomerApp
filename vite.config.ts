7ab6b0f8b36eb1ec66bb55fae4350c37378d5e84
Luan <luan.vieira@avmb.com.br>
2025-02-20 13:18:49 -0300

    Ajuste Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3030
+  },
+  preview: {
+    port: 8080
+  },
   build: {
     rollupOptions: {
       output: {
